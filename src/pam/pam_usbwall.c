3b7978e442875ec62848d683162fd4acc5d2a68d
Arthur d'Avray <arthur.davray@gmail.com>
2016-10-22 22:08:14 +0200

    pam: add first pam module version

---
+++ src/pam/pam_usbwall.c
@@ -0,0 +1,46 @@
+#define PAM_SM_SESSION 1
+
+#include <security/pam_ext.h>
+#include <security/pam_modules.h>
+
+#include <stdio.h>
+
+static int  fetch_debug(int argc, const char **argv);
+static void notify_daemon(void);
+
+PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh __attribute__((unused)),
+                                   int flags          __attribute__((unused)),
+                                   int argc,
+                                   const char **argv)
+{
+  fetch_debug(argc, argv);
+  notify_daemon();
+
+  return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh __attribute__((unused)),
+                                    int flags          __attribute__((unused)),
+                                    int argc,
+                                    const char **argv)
+{
+  fetch_debug(argc, argv);
+  return PAM_SUCCESS;
+}
+
+/************************************
+ * Static functions implementations *
+ ************************************/
+
+static int fetch_debug(int argc, const char **argv)
+{
+  for (int i = 0; i < argc; ++i)
+    puts(argv[i]);
+
+  return 0;
+}
+
+static void notify_daemon(void)
+{
+  puts("ouhlala, je notifie le daemon !");
+}
