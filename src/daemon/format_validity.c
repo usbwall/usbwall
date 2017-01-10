b4a62df4f338634f83074b51b92264114f45f1b6
Mathilde Beylier <mathilde.beylier@thalesgroup.com>
2017-01-10 21:57:58 +0000

    [DAEMON][format_validity] first version of functions for checking string formats

--- src/daemon/format_validity.c
+++ src/daemon/format_validity.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
+
 
 #include "format_validity.h"
 #include "../misc/error_handler.h"
@@ -40,17 +42,193 @@ int32_t check_serial_format(char *serial)
  * - type: 0-9, A-F
  */
 
-int32_t check_vendor_product_format(void)
+int32_t check_vendor_product_format(char *str)
 {
-  /* FIXME */
+  int32_t i = 0;
+
+  if (strlen(str) != LEN_VENDOR_PRODUCT)
+  {
+    return DEVIDD_ERR_OTHER;
+  }
+
+  for (i = 0; i < LEN_VENDOR_PRODUCT; i++)
+  {
+    /* A character is valid if and only if its format is hexadecimal
+       and if it is not a lowercase */
+    if (!isxdigit(str[i]) || islower(str[i]))
+    {
+      return DEVIDD_ERR_OTHER;
+    } 
+  }
   return DEVIDD_SUCCESS;
 }
 
 /* Bcd_device format:
  * - fixed size: 16
  * - type: 0-1
-int32_t check_bcd_format(uint16_t bcd)
+ */
+ int32_t check_bcd_format(char *bcd)
+ { 
+  int32_t i = 0;
+
+  if (strlen(bcd) != LEN_BCD)
+  {
+    return DEVIDD_ERR_OTHER;
+  }
+
+  for (i = 0; i < LEN_BCD; i++)
+  {
+    /* A character is valid if and only if its format is hexadecimal
+       and if it is not a lowercase */
+    if ((bcd[i] != '0') && (bcd[i] != '1'))
+    {
+      return DEVIDD_ERR_OTHER;
+    } 
+  }
+  return DEVIDD_SUCCESS;
+ }
+
+
+int32_t check_machine_format(char *machine)
 {
+  if (machine != LEN_FIELD)
+    return DEVIDD_ERR_OTHER;
+
+  return DEVIDD_SUCCESS;
 }
 
-*/
+int32_t check_bus_port_format(char *str)
+{
+  int32_t i = 0;
+
+  if (strlen(str) > LEN_BUS_PORT)
+  {
+    return DEVIDD_ERR_OTHER;
+  }
+
+  for (i = 0; i < LEN_BUS_PORT; i++)
+  {
+    /* A character is valid if and only if its format is hexadecimal
+       and if it is not a lowercase */
+    if (!isdigit(str[i]))
+    {
+      return DEVIDD_ERR_OTHER;
+    } 
+  }
+  return DEVIDD_SUCCESS;
+}
+
+int32_t check_horaries_format(char *field)
+{
+  int32_t i;
+  uint32_t len; 
+  int32_t dash = DEVIDD_ERR_OTHER;
+
+  len = strlen(field);
+
+  if (len > LEN_FIELD)
+  {
+    return DEVIDD_ERR_OTHER;
+  }
+
+  for (i = 0; i < len; i++)
+  {
+    /* If not a digit, the character is unvalid */
+    if (isdigit(field[i]) == 0)
+    {
+      return DEVIDD_ERR_OTHER;
+    }
+    if ((i != 0) && (i < len - 1) && field[i] == '-')
+    {
+      dash = DEVIDD_SUCCESS;
+    }
+  }
+
+  /* If no dash was found, dash is set to DEVIDD_ERR */ 
+  return dash;
+}
+int32_t check_field_format(char *field, int32_t i)
+{
+  switch(i)
+  {
+    case FIELD_MACHINE:
+      check_machine_format(field);
+      break;
+
+    case FIELD_BUS:
+    case FIELD_PORT:
+      check_bus_port_format(field);
+      break;
+
+    case FIELD_SERIAL:
+      check_serial_format(field);
+      break;
+    case FIELD_VENDOR:
+    case FIELD_PRODUCT:
+      check_vendor_product_format(field);
+      break;
+    case FIELD_BCD:
+      check_bcd_format(field);
+      break;
+    case FIELD_HORARY:
+      check_horaries_format(field);
+      break;
+  }
+}
+
+/* Rule format: 
+   (accept:user):machine:bus:port:serial:vendor:product:bcd:horary
+   - 8 fields
+   - length field: max 64
+ */ 
+int32_t check_rule_format(char *rule)
+{
+  int32_t i = 0; 
+  char *token;
+  uint32_t field = 1;
+  int32_t valid = DEVIDD_SUCCESS;
+
+  token = malloc(DEVID_MAX_LEN);
+  if (token == NULL)
+  {
+    return DEVIDD_ERR_MEM;
+  }
+
+  for (i = 0; token != NULL; i++)
+  {
+    if (i == 0)
+    {
+      token = strtok(rule, ":");
+    }
+    else
+    {
+      token = strtok(NULL, ":");
+    }
+
+    check_field_format(token, i);
+
+    if (strlen(token) > LEN_FIELD)
+    {
+      syslog(LOG_ERR, "Rule %s invalid: field "%s" is too long",
+             rule, token);
+      valid = DEVIDD_ERR_OTHER;
+      break; 
+    }
+  }
+  if (i != NB_FIELD_COMPLETE_ID)
+  {
+    syslog(LOG_ERR, "Rule %s invalid: %d fields found, 7 expected",
+           rule, (i + 1));
+    valid = DEVIDD_ERR_OTHER;
+  }
+
+  free(token);
+
+  return valid; 
+}
+
+int32_t check_complete_id_format(char *complete_id)
+{
+   
+}
+
--- src/daemon/format_validity.h
+++ src/daemon/format_validity.h
@@ -2,14 +2,35 @@
 # define FORMAT_VALIDITY_H
 
 #include <stdint.h> 
- 
+
+# define LEN_FIELD 64
+# define NB_FIELD_COMPLETE_ID 8
+# define DEVID_MAX_LEN 1024
+# define MIN_SERIAL 12
+# define MAX_SERIAL 126
+# define LEN_VENDOR_PRODUCT 4
+# define LEN_BCD 16
+# define MAX_BUS_PORT 3
+
+# define FIELD_MACHINE 0
+# define FIELD_BUS 1
+# define FIELD_PORT 2
+# define FIELD_SERIAL 3
+# define FIELD_VENDOR 4
+# define FIELD_PRODUCT 5
+# define FIELD_BCD 6
+# define FIELD_HORARY 7
+
+
+
 int32_t check_serial_format(char *serial);
 
-int32_t check_vendor_product_format(void); /* uint16_t id */
+int32_t check_vendor_product_format(char *str); /* uint16_t id */
 
-int32_t check_bcd_format(uint16_t bcd);
+int32_t check_bcd_format(char *bcd);
 
-/* Notice that bus and port doesn't need to be checked: the only rule is that they must have less than 3 digits. But uint8_t already forces them to be so. */
+/* Notice that bus and port doesn't need to be checked: the only rule is that
+they must have less than 3 digits. But uint8_t already forces them to be so. */
 
 
 #endif /* !FORMAT_VALIDITY_H */
