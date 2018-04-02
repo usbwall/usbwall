aceed93979fe073236ba055cdbaa421d9f7027a2
Luc Deniau <luc.deniau@edu.esiee.fr>
2018-04-02 18:42:12 +0200

    Adding parser, format validity and main unit tests.

--- src/test/daemon_main_test.c
+++ src/test/daemon_main_test.c
@@ -1,29 +1,54 @@
+#include <string.h>
 #include <stdio.h>
-
-#define CTEST_MAIN
-#define CTEST_SEGFAULT
-
+#include <stdlib.h>
 #include "ctest.h"
 
+#include "../daemon/daemon_main.c"
+
 /**
-Compilation tests:
-make -f MakefileTest clean && make -f MakefileTest all
+ * \Daemon Main unit tests 
+ */
 
-Éxécution test:
-./usbwall_test
-Pas de colorisation pour l'utilisation en pipe/file.
-Retourne le nombre de test KO, 0 si tout est OK
+CTEST(main, create_pidfile) {
+	char pid_path[] = "/tmp/usbwall_test_pid";
+	remove(pid_path);
+	int ret = create_pidfile(pid_path);
+	ASSERT_NOT_EQUAL(-1, ret);
 
-./usbwall_test parser permet de lancer les tests du module parser
-
-Besoin de CLANG pour la compilation de ctest
-*/
-
-int main(int argc, const char *argv[])
-{
-	printf("\n=== Starting USBWall testing suites. ===\n\n");
-	int result = ctest_main(argc, argv);
-	return result;
+	char buffer[100];
+	FILE *fp = fopen(pid_path, "r");
+	while (fgets(buffer, sizeof(buffer), fp)) {
+		ASSERT_EQUAL(getpid(), atoi(buffer));
+	}
+	fclose(fp);
+	remove(pid_path);
 }
 
+CTEST(main, remove_pidfile) {
+	char pid_path[] = "/tmp/usbwall_test_pid_remove";
+	FILE *fp = fopen(pid_path, "ab+");
+	if( access( pid_path, F_OK ) != -1 ) {
+		remove_pidfile(pid_path, fileno(fp));
+		if( access( pid_path, F_OK ) != -1 ) {
+			CTEST_ERR("Couldn't remove pidfile: %d", fileno(fp));
+			ASSERT_FAIL();
+		}
+	} else {
+		CTEST_ERR("Couldn't create dummy pidfile for deletion test.");
+		ASSERT_FAIL();
+	}
+}
 
+/**
+Un-testable as is.
+CTEST_SKIP(main, daemonize) {
+}
+*/
+
+/**
+Un-testable as is.
+CTEST_SKIP(main, parse_args) {
+	char *str1[1];
+	str1[0] = "-h";
+}
+*/
---
+++ src/test/usbwall_main_test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define CTEST_MAIN
+#define CTEST_SEGFAULT
+
+#include "ctest.h"
+
+/**
+Compilation tests:
+make -j
+
+Éxécution test:
+out/usbwall_test
+Pas de colorisation pour l'utilisation en pipe/file.
+Retourne le nombre de test KO, 0 si tout est OK
+
+./usbwall_test parser permet de lancer les tests du module parser
+
+Besoin de CLANG pour la compilation de ctest
+*/
+
+int main(int argc, const char *argv[])
+{
+	printf("\n=== Starting USBWall testing suites. ===\n\n");
+	int result = ctest_main(argc, argv);
+	return result;
+}
+
+
