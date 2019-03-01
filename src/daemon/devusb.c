68cc83ea7b8d60b9ae59f1a3a54beaaff3af2a6c
Jérôme Maurin <jerome.maurin@eternilab.com>
2019-03-01 19:59:58 +0100

    Use of libusb_set_debug is deprecated, replaced by libusb_set_option

--- src/daemon/devusb.c
+++ src/daemon/devusb.c
@@ -420,7 +420,7 @@ int init_devusb(void)
    * \todo
    * FIXME ????
    */
-  libusb_set_debug(NULL, LIBUSB_LOG_LEVEL_WARNING);
+  libusb_set_option(NULL, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_WARNING);
 
   g_wait_for_hotplugs = 1;
   int rcode =
